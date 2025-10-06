76101cc91bb24ed99a85582e1f0f5526ba818afe
mohammedosmanghori <ghorimohammedosman@gmail.com>
2025-10-06 21:28:20 +0530

    Add files via upload

---
+++ TwoDArrays.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class TwoDArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows : ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns : ");
+        int cols = sc.nextInt();
+
+
+        int[][] numbers = new int[rows][cols];
+        int [][] transpose = new int[cols][rows];
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+               numbers[i][j]=sc.nextInt();
+
+            }
+
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                transpose[j][i]=numbers[i][j];
+
+            }
+        }
+        for(int i=0;i<cols;i++){
+            for(int j=0;j<rows;j++){
+
+             System.out.print(transpose[i][j]+ " ");
+            }
+            System.out.println();
+            
+        }
+        
+    }
+    
+}
